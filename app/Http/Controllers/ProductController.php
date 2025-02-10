c3862da7b2580a10f000a179b05057d02dac7a5f
ahsandev0 <bestkhan92@gmail.com>
2025-02-10 12:47:30 +0500

    Product Controller , Model and migration added

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use App\Models\Product;
+use Illuminate\Support\Facades\Storage;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::orderBy('created_at', 'desc')->get();
+        $total_qty = $products->sum('quantity');
+        $total_val = $products->sum('total_value');
+        return view('products.index', compact('products','total_qty','total_val'));
+    }
+
+    public function store(Request $request)
+    {
+        $product = $this->insertOrUpdate($request);
+        return response()->json($product);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $product = $this->insertOrUpdate($request,$id);
+
+        return response()->json($product);
+    }
+
+    private function insertOrUpdate($request,$id=0){
+
+        $request->validate([
+            'name' => 'required|string',
+            'quantity' => 'required|integer|min:1',
+            'price' => 'required|numeric|min:1'
+        ]);
+
+        if($id > 0){
+            $product = Product::findOrFail($id);
+            $product->update([
+                'name' => $request->name,
+                'quantity' => $request->quantity,
+                'price' => $request->price,
+                'total_value' => $request->quantity * $request->price
+            ]);
+        }else{
+            $product = Product::create([
+                'name' => $request->name,
+                'quantity' => $request->quantity,
+                'price' => $request->price,
+                'total_value' => $request->quantity * $request->price,
+                'created_at' => Carbon::now()
+            ]);
+        }
+        $this->saveToJson();
+
+        return $product;
+    }
+
+    private function saveToJson()
+    {
+        $products = Product::all();
+
+        $jsonData = $products->toJson(JSON_PRETTY_PRINT);
+
+        Storage::disk('public')->put('products.json', $jsonData);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [ProductController::class, 'index']);
+Route::post('/store', [ProductController::class, 'store']);
+Route::post('/update/{id}', [ProductController::class, 'update']);
+
+Route::get('/download-json', function () {
+    $filePath = storage_path('app/public/products.json');
+
+    if (!file_exists($filePath)) {
+        $message = 'File not found at'.$filePath;
+        //dd($message);
+        abort(404, $message);
+    }
+
+    return Response::download($filePath, 'products.json');
 });
